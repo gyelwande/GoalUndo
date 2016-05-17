3689b5db9913f6acaf788e6bc96bc3e7e73c5c3d
september <gyelwande@mail.csuchico.edu>
2016-05-17 00:42:20 -0700

    file commited for partial changes

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
- 
+
 class GoalUndoTest : public ::testing::Test
 {
 	protected:
@@ -17,4 +17,71 @@ class GoalUndoTest : public ::testing::Test
 TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(GoalUndoTest, checkForBlankOperationName)
+{
+  GoalUndo obj;
+  obj.addOperation("firstGoal","");
+	ASSERT_EQ("",obj.getGoal());
+}
+
+TEST(GoalUndoTest, checkForValidOperationAndGoalName)
+{
+  GoalUndo obj;
+  obj.addOperation("firstGoal","firstOP");
+	ASSERT_EQ("firstGoal",obj.getGoal());
+}
+
+
+TEST(GoalUndoTest, checkForBlankGoalName)
+{
+  GoalUndo obj;
+  obj.addOperation("","firstOp");
+	ASSERT_EQ("",obj.getGoal());
+}
+
+TEST(GoalUndoTest, checkAddOperationWithBlankGoalAndOperationName)
+{
+  GoalUndo obj;
+  obj.addOperation("","");
+	ASSERT_EQ("",obj.getGoal());
+}
+
+TEST(GoalUndoTest, checkAllAddOpeartionsWithValidParameters)
+{
+  GoalUndo obj;
+  obj.addOperation("goal1","op1");
+  obj.addOperation("op2");
+	ASSERT_EQ("op1 op2",obj.getOperations());
+}
+
+TEST(GoalUndoTest, checkForAddOp2WithEmptyGoalName)
+{
+  GoalUndo obj;
+  obj.addOperation("op1");
+	ASSERT_EQ("op1",obj.getGoal());
+}
+
+TEST(GoalUndoTest, checkForTwoAddOpeartionsWithOneEmptyOperationName)
+{
+  GoalUndo obj;
+  obj.addOperation("goal1","op1");
+  obj.addOperation("");
+	ASSERT_EQ("op1",obj.getOperations());
+}
+
+TEST(GoalUndoTest, checkForAddOp2WithEmptyOperationName)
+{
+  GoalUndo obj;
+  obj.addOperation("");
+	ASSERT_EQ("",obj.getOperations());
+}
+
+TEST(GoalUndoTest, checkForUndoOperation)
+{
+  GoalUndo obj;
+  obj.addOperation("goal1","op1");
+  obj.undoOperation();
+	ASSERT_EQ("",obj.getOperations());
 }
